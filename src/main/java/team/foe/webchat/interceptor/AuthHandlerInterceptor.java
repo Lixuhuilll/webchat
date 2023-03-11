f911b4095630ddae33033f9332339caa7e73fdf1
李旭辉 <676824363@qq.com>
2023-03-11 21:33:18 +0800

    修改注释

--- src/main/java/team/foe/webchat/interceptor/AuthHandlerInterceptor.java
+++ src/main/java/team/foe/webchat/interceptor/AuthHandlerInterceptor.java
@@ -1,6 +1,5 @@
 package team.foe.webchat.interceptor;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
