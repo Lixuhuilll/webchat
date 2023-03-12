65bbd6a9dbf0e0abfdef0f42150128d238c9e237
李旭辉 <676824363@qq.com>
2023-03-12 09:06:08 +0800

    使用文本块

--- src/main/java/team/foe/webchat/mapper/UserMapper.java
+++ src/main/java/team/foe/webchat/mapper/UserMapper.java
@@ -23,12 +23,13 @@ public interface UserMapper {
     /**
      * 建表语句，表不存在才有效
      */
-    @Update("CREATE TABLE IF NOT EXISTS `users` (\n" +
-            "  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id，主键',\n" +
-            "  `name` varchar(100) NOT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(100) NOT NULL COMMENT '用户密码，BCrypt混淆',\n" +
-            "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `users_name_IDX` (`name`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COMMENT='用户基础表';")
+    @Update("""
+            CREATE TABLE IF NOT EXISTS `users` (
+              `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id，主键',
+              `name` varchar(100) NOT NULL COMMENT '用户名',
+              `password` varchar(100) NOT NULL COMMENT '用户密码，BCrypt混淆',
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `users_name_IDX` (`name`) USING BTREE
+            ) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COMMENT='用户基础表';""")
     void creatUsersTable();
 }
